47b44852ae121854b713a9389e6a1f62f4a214de
nathanhall762 <nathanhall762@gmail.com>
2024-01-19 17:30:44 -0600

    fixed emotion object again

--- better-belt/src/components/EmotionsObject.js
+++ better-belt/src/components/EmotionsObject.js
@@ -1,11 +1,21 @@
 const EmotionsObject = {
-    "Angry": {
+    "Fearful": {
       "Scared": ["Helpless", "Frightened"],
       "Anxious": ["Overwhelmed", "Worried"],
       "Insecure": ["Inadequate", "Inferior"],
       "Weak": ["Worthless", "Insignificant"],
       "Rejected": ["Excluded", "Persecuted"],
       "Threatened": ["Nervous", "Exposed"]
+    },
+    "Angry": {
+      "Let Down": ["Betrayed", "Resentful"],
+      "Humiliated": ["Disrespected", "Ridiculed"],
+      "Bitter": ["Indignant", "Violated"],
+      "Mad": ["Furious", "Jealous"],
+      "Aggressive": ["Provoked", "Hostile"],
+      "Frustrated": ["Infuriated", "Annoyed"],
+      "Distant": ["Withdrawn", "Numb"],
+      "Critical": ["Skeptical", "Dismissive"],
     },
     "Disgusted": {
       "Disapproving": ["Judgmental", "Embarrassed"],
